713019278e80a709d9cd73b0f1950abf11bb9249
Christian <christiansmith2394@gmail.com>
2022-10-18 14:01:06 -0400

    File clean up and components folder started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <nav>
+            <a href='#'>Home</a>
+            <a href='#'>Services</a>
+            <a href='#'>About</a>
+            <a href='#'>Contact</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
